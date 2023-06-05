817c1a294c46c5b13c102df95c16a6a7f82b4013
RokwonK <wonrok97@naver.com>
2023-06-05 12:52:54 +0900

    ✨ terrform 환경 구성

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "1.4.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "ap-northeast-2"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "mentos"
+}
+
+provider "aws" {
+  alias                    = "virginia"
+  region                   = "us-east-1"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "mentos"
+}
