a3fd8979db094c0c2a9846a51fd15366eeff732c
RokwonK <wonrok97@naver.com>
2023-06-05 12:52:38 +0900

    ✨ 전체 변수 정의

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "az_names" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+
+}
+
+variable "private_subnets" {
+
+}
