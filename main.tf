8cb60eff81a7608302efe95c456d5babdf251898
RokwonK <wonrok97@naver.com>
2023-06-05 17:29:12 +0900

    ✨ subdomain acm 생성

--- main.tf
+++ main.tf
@@ -18,11 +18,19 @@ module "ecr" {
   name = var.project_name
 }
 
-# Route53
+# Route53 - Hosted Zone
+module "route53_zone" {
+  source = "./modules/route53/zone"
+  name = var.domain_name
+}
 
 # ACM
+module "acm" {
+  source = "./modules/acm"
+  domain_name = var.sub_domain
+}
 
-# ALB - ACM, DNS, sg, target group
+# ALB - ACM등록, DNS등록, sg등록, target group등록
 
 
 # ECS - ALB 연결, sg(inbound - ALB sg, 22)
