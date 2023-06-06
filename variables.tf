8dfb01233450980719a1aee45549eb2a3b77da47
RokwonK <wonrok97@naver.com>
2023-06-06 21:42:20 +0900

    🎨 루트 모듈 변수 수정

--- variables.tf
+++ variables.tf
@@ -1,23 +1,21 @@
 variable "project_name" {
   type = string
 }
-variable "domain_name" {
-  type = string
-}
-variable "sub_domain" {
-  type = string
-}
+variable "domain_name" { }
+variable "sub_domain" { }
+variable "api_server_domain" { }
 
 
 variable "vpc_cidr" {
   type = string
 }
-variable "az_names" {
-  type = list(string)
-}
+variable "az_names" { }
+
 variable "public_subnets" { }
 variable "private_subnets" { }
+
 variable "alb_inbound_rule" { }
-variable "ecs_inbound_rule" { }
+# variable "ecs_service_inbound_rule" { }
+# variable "ecs_instance_inbound_rule" { }
 variable "db_inbound_rule" { }
 variable "outbound_rule" { }
