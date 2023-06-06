0452e7dc05cc52b87c2ad26dcb21de6847a9155e
RokwonK <wonrok97@naver.com>
2023-06-06 21:40:39 +0900

    ✨ alb listener, target_group 등 추가

---
+++ modules/alb/main.tf
@@ -0,0 +1,56 @@
+resource "aws_alb" "alb" {
+  load_balancer_type = "application"
+  security_groups = [ var.security_group_id ]
+  subnets = var.subnet_ids
+
+  # enable_deletion_protection = true
+  tags = merge(
+    {
+      Name = format(
+        "%s-alb",
+        var.name,
+      )
+    },
+    var.tags
+  )
+}
+
+resource "aws_alb_listener" "https_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  certificate_arn = var.acm_arn
+  port = 443
+  protocol = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_alb_listener" "http_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port = 443
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_alb_target_group" "alb_target_group" {
+  name = "${var.name}-alb-target-group"
+  vpc_id = var.vpc_id
+  target_type = "ip"
+  protocol = "HTTP"
+  port = var.forwarding_port
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ modules/alb/variables.tf
@@ -0,0 +1,12 @@
+variable "name" { }
+variable "tags" { }
+
+variable "acm_arn" { }
+
+variable "vpc_id" { }
+
+variable "security_group_id" { }
+
+variable "subnet_ids" { }
+
+variable "forwarding_port" {}
