4707e5b10f00620995a76b8ff920fa82681683ee
RokwonK <wonrok97@naver.com>
2023-06-08 22:19:06 +0900

    🎨 EC2 초기 설정 - ECS Agent 설치 및 시작 명령어 AMI에 맞춰 수정

--- modules/ecs/ec2_user_data/user_data.sh
+++ modules/ecs/ec2_user_data/user_data.sh
@@ -2,10 +2,13 @@
 
 # Update all packages
 sudo yum update -y
-sudo yum install -y ecs-init
-sudo service docker start
-sudo start ecs
+sudo amazon-linux-extras disable docker
+sudo amazon-linux-extras install -y ecs
 
-#Adding cluster name in ecs config
-echo ECS_CLUSTER=mentos-ecs-cluster >> /etc/ecs/ecs.config
+# Adding cluster name in ecs config
+sudo mkdir -p /etc/ecs && sudo touch /etc/ecs/ecs.config
+sudo echo ECS_CLUSTER=mentos-ecs-cluster >> /etc/ecs/ecs.config
 cat /etc/ecs/ecs.config | grep "ECS_CLUSTER"
+
+# Start ECS Agent
+sudo systemctl enable --now ecs
