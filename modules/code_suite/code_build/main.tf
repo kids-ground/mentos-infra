066c403dac4d23f5ab1c7a434e1da4705013bf4c
RokwonK <wonrok97@naver.com>
2023-06-09 23:40:24 +0900

    ✨ Codebuild project 모듈 추가

---
+++ modules/code_suite/code_build/main.tf
@@ -0,0 +1,32 @@
+resource "aws_codebuild_project" "codebuild" {
+  name = "${var.name}-codebuild-project"
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/standard:5.0"
+    type = "LINUX_CONTAINER"
+    privileged_mode = true
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "REPOSITORY_URI"
+      value = var.ecr_repo_uri
+    }
+    environment_variable {
+      name  = "CONTAINER_NAME"
+      value = var.container_name
+    }
+  }
+
+  source {
+    type = "CODEPIPELINE"
+    buildspec = <<BUILDSPEC
+    ${file("${path.module}/buildspec.yml")}
+    BUILDSPEC
+  }
+}
