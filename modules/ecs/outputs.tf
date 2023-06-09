1dde70e0e69502bc9077f0fa30d48bb8da677399
RokwonK <wonrok97@naver.com>
2023-06-10 01:27:05 +0900

    ✨ ecs output - cluster, service 추가

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,3 +1,11 @@
 output "ecs_instance_id" {
   value = aws_instance.ec2.id
 }
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.ecs_cluster.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.service.name
+}
