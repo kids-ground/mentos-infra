e3fef4fd208d4d89aac3dc4f89e8ce2df1a6dbea
RokwonK <wonrok97@naver.com>
2023-06-12 12:02:52 +0900

    ✨ ecr image life cycle policy 추가 - 태그없는 이미지 최대 5개까지만 유지

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -6,3 +6,26 @@ resource "aws_ecr_repository" "ecr" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep last 5 images",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
