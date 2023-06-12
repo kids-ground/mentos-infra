ac4fa754daf275d975162637db5c9bcbcc616c7e
RokwonK <wonrok97@naver.com>
2023-06-12 11:54:11 +0900

    🎨 alb target group - target type ip -> instance로 변경

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,6 +1,8 @@
 resource "aws_alb" "alb" {
   load_balancer_type = "application"
-  security_groups = [ var.security_group_id ]
+  security_groups = [ 
+    var.security_group_id
+  ]
   subnets = var.subnet_ids
 
   # enable_deletion_protection = true
@@ -44,22 +46,22 @@ resource "aws_alb_listener" "http_listener" {
 }
 
 resource "aws_alb_target_group" "alb_target_group" {
-  name = "${var.name}-alb-target-group"
+  name = "${var.name}-alb-target-group-2"
   vpc_id = var.vpc_id
-  target_type = "ip"
+  target_type = "instance"
   protocol = "HTTP"
   port = var.forwarding_port
   
   health_check {
     healthy_threshold   = "3"
     interval            = "10"
-    port                = "80"
+    port                = "traffic-port"
     path                = "/health-check"
     protocol            = "HTTP"
     unhealthy_threshold = "3"
   }
 
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = false
   }
 }
