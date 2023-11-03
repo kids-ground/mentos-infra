07f1ec70becc7862683e7ab424082cf92cae192a
RokwonK <wonrok97@naver.com>
2023-11-03 21:49:07 +0900

    java17 build를 위한 codebuild OS Iamge 5.0 -> 6.0 으로 업그레이드

--- modules/code_suite/code_build/main.tf
+++ modules/code_suite/code_build/main.tf
@@ -8,7 +8,7 @@ resource "aws_codebuild_project" "codebuild" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image = "aws/codebuild/standard:5.0"
+    image = "aws/codebuild/standard:6.0"
     type = "LINUX_CONTAINER"
     privileged_mode = true
     image_pull_credentials_type = "CODEBUILD"
